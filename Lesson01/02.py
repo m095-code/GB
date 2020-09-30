57cb74618fdcca6a9db9b43defadcefcf3169cd0
koks <m095_07@mail.ru>
2020-09-30 16:22:58 +0300

    отформатировал 2 задание

--- Lesson01/02.py
+++ Lesson01/02.py
@@ -6,10 +6,11 @@ sec = int(input('Введите число в секундах : '))
 minute = 0
 hour = 0
 while sec >= 60:
-    minute = sec / 60
+    minute = sec // 60
     sec = sec % 60
     while minute >= 60:
-        hour = minute / 60
+        hour = minute // 60
         minute = minute % 60
 
 print(int(hour), ':', int(minute), ':', int(sec))
+print(f'{hour:>02}:{minute:>02}:{sec:>02}')
